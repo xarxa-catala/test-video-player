4c50b6b585c14513c72c71950506f3d7d972a39b
Agusti Montes Salas <a.gus493@gmail.com>
2020-04-13 11:47:40 +0200

    Afegida variable per a canviar el data-source fàcilment.

--- js/video_player.js
+++ js/video_player.js
@@ -2,16 +2,17 @@ var video;
 var configVideoPlayerId = 'video_player';
 var configChaptersListId = 'video_player_chapters';
 var configChapterClass = 'video_player_chapter';
+var configDataSource = 'vsource';
 
 $(document).ready(function () {
   video = document.getElementById(configVideoPlayerId);
   let $vidosrc = $('#' + configChaptersListId + ' li:first-child .' + configChapterClass);
-  loadVideo($vidosrc.data('vsource'));
+  loadVideo($vidosrc.data(configDataSource));
 });
 
 $(document).on('click', '#' + configChaptersListId + ' .' + configChapterClass, function (e) {
   e.preventDefault();
-  loadVideo($(this).data('vsource'));
+  loadVideo($(this).data(configDataSource));
   video.play();
 });
 
